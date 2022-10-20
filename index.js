9a29f78fa05d83d58031308e8bc4b15ef390fdbe
jwhitlow45 <jwhitlow45@gmail.com>
2022-10-20 11:49:27 -0700

    feat:implement final search parameters

--- index.js
+++ index.js
@@ -6,21 +6,28 @@ class PokemonDB {
     this.collection = collection;
   }
 
+  findNumber(num) {
+    let newColl = new Array();
+    for (let poke of this.collection) {
+      if (poke.Num == num) {
+        newColl.push(poke);
+      }
+    }
+    return new PokemonDB(newColl);
+  }
+
   findName(name) {
     let newColl = new Array();
-
     for (let poke of this.collection) {
       if (poke.Name.toLowerCase().includes(name.toLowerCase())) {
         newColl.push(poke);
       }
     }
-
     return new PokemonDB(newColl);
   }
 
   findType(type) {
     let newColl = new Array();
-
     for (let poke of this.collection) {
       if (poke.Type1.toLowerCase() == type.toLowerCase()) {
         newColl.push(poke);
@@ -28,23 +35,40 @@ class PokemonDB {
         newColl.push(poke);
       }
     }
+    return new PokemonDB(newColl);
+  }
 
+  findStat(stat, min, max) {
+    let newColl = new Array();
+    for (let poke of this.collection) {
+      if (poke[stat] >= min && poke[stat] <= max) {
+        newColl.push(poke);
+      }
+    }
+    return new PokemonDB(newColl);
+  }
+
+  findGeneration(gen) {
+    let newColl = new Array();
+    for (let poke of this.collection) {
+      if (poke.Generation == gen) {
+        newColl.push(poke);
+      }
+    }
     return new PokemonDB(newColl);
   }
 
   findLegendary(legendary) {
     let newColl = new Array();
-
     for (let poke of this.collection) {
       if (poke.Legendary.toLowerCase() == legendary.toLowerCase()) {
         newColl.push(poke);
       }
     }
-
     return new PokemonDB(newColl);
   }
 }
 
 let db = new PokemonDB(pokemon);
 
-console.log(db.findName("char"));
+console.log(db.findNumber(10));
