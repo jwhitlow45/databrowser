a0dde8a3a796635dced1a1aaa724eb58f867f5f2
jwhitlow45 <jwhitlow45@gmail.com>
2022-12-04 16:08:44 -0800

    feat:setup simple query to db

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ const insert_button = document.getElementById('insert-button');
 const delete_button = document.getElementById('delete-button');
 const query_form = document.getElementById('query-form');
 const insert_form = document.getElementById('insert-form');
+const query_submit = document.getElementById('query-submit');
+const insert_submit = document.getElementById('insert-submit');
 
 query_button.addEventListener('click', showQueryForm);
 insert_button.addEventListener('click', showInsertForm);
+query_submit.addEventListener('click', populateDataBrowser);
+insert_submit.addEventListener('click', insertData);
+
+async function handleFetch(url, form) {
+  let formData = new FormData(form);
+  let result = await fetch(url, {
+    method:'POST',
+    body: formData
+  }).then(response => {
+    return response.json();
+  }).catch(err => {
+    console.log(err); 
+  });
+  return result;
+}
 
 function showQueryForm() {
   insert_form.hidden = 1;
@@ -15,4 +32,14 @@ function showQueryForm() {
 function showInsertForm() {
   insert_form.hidden = 0;
   query_form.hidden = 1;
+}
+
+function insertData() {
+
+}
+
+async function populateDataBrowser(event) {
+  event.preventDefault();
+  let result = await handleFetch('./php/query.php', query_form);
+  console.log(result);
 }
--- php/query.php
+++ php/query.php
@@ -2,4 +2,31 @@
 include './dbconnect.php';
 
 $conn = db_connect();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $sql = 'SELECT * from pokemon';
+    $pokemon_arr = array();
+    if ($stmt = mysqli_prepare($conn, $sql)) {
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        while ($row = mysqli_fetch_row($result)) {
+            array_push($pokemon_arr, [
+                'number' => $row[0],	
+                'name' => $row[1],	
+                'type1' => $row[2],	
+                'type2' => $row[3],	
+                'hp' => $row[4],	
+                'attack' => $row[5],	
+                'defense' => $row[6],	
+                'spatk' => $row[7],	
+                'spdef' => $row[8],	
+                'speed' => $row[9],	
+                'generation' => $row[10],	
+                'legendary' => $row[11]
+            ]);
+        }
+    }
+    mysqli_stmt_close($stmt);
+    echo json_encode($pokemon_arr);
+}
 ?>
