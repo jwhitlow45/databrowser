8127249297086feaea70e758e3a9bb9339724c58
jwhitlow45 <jwhitlow45@gmail.com>
2022-12-04 19:49:18 -0800

    feat:display data to databrowser

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ query_submit.addEventListener('click', getData);
 insert_submit.addEventListener('click', insertData);
 resetdb_submit.addEventListener('click', resetDB);
 
+let pokemon_arr = [];
+let index = -1;
+
 async function handleFetch(url, form) {
   let formData;
   if (form == null) {
@@ -68,4 +71,27 @@ async function deleteData(number) {
 async function getData() {
   let result = await handleFetch('./php/query.php', query_form);
   console.log(result);
+  return result;
+}
+
+async function populateDataBrowser() {
+  pokemon_arr = await getData();
+  index = 0;
+}
+
+function displayPokemon() {
+  const image = document.getElementById('pokemon-image');
+  let pokemon = pokemon_arr[index];
+  image.src = './data/images/' + pokemon['number'] + '.png';
+
+  for (let stat in pokemon) {
+    if (pokemon[stat] !== '') {
+      const cur_stat = document.getElementById(stat);
+      if (stat == 'legendary') {
+        cur_stat.innerText = pokemon[stat] === 1 ? 'true' : 'false';
+        continue;
+      }
+      cur_stat.innerText = pokemon[stat];
+    }
+  }
 }
