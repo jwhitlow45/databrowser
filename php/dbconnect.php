73dfbac35de87deb924ba9f013b1f9f5b62354a8
jwhitlow45 <jwhitlow45@gmail.com>
2022-12-07 14:48:10 -0800

    feat:readonly table now constructed from csv on page load

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -28,9 +28,50 @@ function ensure_db_exists() {
     $conn->query($sql);
 }
 
+function ensure_main_table_exists() {
+    $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+
+    $table_sql = 'CREATE TABLE IF NOT EXISTS pokemon_original (
+        number INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(18) NOT NULL,
+        type1 VARCHAR(8) NOT NULL,
+        type2 VARCHAR(8),
+        hp INT NOT NULL,
+        attack INT NOT NULL,
+        defense INT NOT NULL,
+        spatk INT NOT NULL,
+        spdef INT NOT NULL,
+        speed INT NOT NULL,
+        generation INT NOT NULL,
+        legendary TINYINT NOT NULL
+    );';
+
+    if ($stmt = mysqli_prepare($conn, $table_sql)) {
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+    }
+
+    $check_empty_query = 'SELECT * FROM pokemon_original';
+    $results = mysqli_query($conn, $check_empty_query);
+    if (mysqli_num_rows($results) == 0) {
+        $csv_path = '../data/pokemon_original.csv';
+        $csv_file = fopen($csv_path, "r");
+        while (($row = fgetcsv($csv_file)) !== FALSE) {
+            $stmt = $conn->prepare(
+                'INSERT INTO pokemon_original (
+                    number, name, type1, type2, hp, attack, defense, spatk, spdef, speed, generation, legendary)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+                ');
+            $stmt->bind_param("isssiiiiiiii", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8], $row[9], $row[10], $row[11],);
+            $stmt->execute();
+        }
+    }
+}
+
 function db_connect() {
     parse_db_credentials();
     ensure_db_exists();
+    ensure_main_table_exists();
 
     $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
     ensure_tables_exist($conn);
